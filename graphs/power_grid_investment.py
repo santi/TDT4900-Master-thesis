d291d4fd1fb3d0ba31394e589bb096dae34be586
Vemund Santi <vemund@santi.no>
2018-10-26 14:07:56 +0200

    feat: add power_grid_incvestment graphs

---
+++ graphs/power_grid_investment.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [*range(1997,2019)]
+y = [4856, 5234, 5246, 4668, 5392,6215,7881,8931,8325,10248,11918,14442,12606,14612,16873,19178,20422,21677,23114,26222,31396,41737]
+
+plt.plot(x, y)
+plt.yticks([0, 10000, 20000, 30000, 40000])
+plt.xticks([*range(1997,2019, 2)])
+
+plt.grid()
+
+plt.xlabel('Year')
+plt.ylabel('Million NOK')
+plt.show()
+
